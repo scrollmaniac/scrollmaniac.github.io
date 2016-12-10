6518cf88442f9a136b569895cc3a0e0eb85dbded
Antonio Trento <antoniotrento83@gmail.com>
2016-12-10 16:31:49 +0100

    s

--- assets/js/scrollmaniac/demo5-test.js
+++ assets/js/scrollmaniac/demo5-test.js
@@ -200,6 +200,8 @@ $(function () { // wait for document ready
 
   // define movement of panels
   var wipeAnimation = new TimelineMax()
+    .fromTo("section.panel.turqoise", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
+    .fromTo("section.panel.green",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
     .fromTo("section.panel.bordeaux", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
 
   // create scene to pin and link animation
