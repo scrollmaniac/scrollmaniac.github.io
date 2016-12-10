a6e6ad3b50e1bfe99438067e627d348cb0a9c9c2
Antonio Trento <antoniotrento83@gmail.com>
2016-12-10 16:27:02 +0100

    s

--- assets/js/scrollmaniac/demo5-test.js
+++ assets/js/scrollmaniac/demo5-test.js
@@ -191,3 +191,25 @@
         })
     }
 }(jQuery);
+
+
+
+$(function () { // wait for document ready
+  // init
+  var controller = new ScrollMagic.Controller();
+
+  // define movement of panels
+  var wipeAnimation = new TimelineMax()
+    .fromTo("section.panel.bordeaux", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
+
+  // create scene to pin and link animation
+  new ScrollMagic.Scene({
+      triggerElement: "#pinContainer",
+      triggerHook: "onLeave",
+      duration: "300%"
+    })
+    .setPin("#pinContainer")
+    .setTween(wipeAnimation)
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+});
